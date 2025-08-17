19a7e5325d8625e95b3290cfc0af6ae34c9c9b37
soham-kadam-06 <sohamkadam440@gmail.com>
2025-08-17 15:28:06 +0530

    Create MiniProject.java

---
+++ MiniProject.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class MiniProject {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Mini Project!");
+        // Your code logic here
+    }
+}
+add
